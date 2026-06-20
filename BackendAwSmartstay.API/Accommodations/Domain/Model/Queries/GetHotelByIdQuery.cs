8ca9e91bcd628c13a1a243935627fd9a168185f6
u20211d760 <u20211d760@upc.edu.pe>
2026-06-20 11:16:19 -0500

    docs(accommodations): add documentation to GetHotelByIdQuery

--- BackendAwSmartstay.API/Accommodations/Domain/Model/Queries/GetHotelByIdQuery.cs
+++ BackendAwSmartstay.API/Accommodations/Domain/Model/Queries/GetHotelByIdQuery.cs
@@ -1,3 +1,7 @@
 namespace BackendAwSmartstay.API.Accommodations.Domain.Model.Queries;
 
+/// <summary>
+/// Query to retrieve all rooms for a specific hotel.
+/// </summary>
+/// <param name="HotelId"></param>
 public record GetHotelByIdQuery(int HotelId);
