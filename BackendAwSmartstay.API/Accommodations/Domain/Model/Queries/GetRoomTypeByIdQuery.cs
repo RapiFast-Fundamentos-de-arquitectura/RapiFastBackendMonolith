fbcd31683fca1009a5f1a858ea07b808dd2d1c1d
u20211d760 <u20211d760@upc.edu.pe>
2026-06-20 11:19:21 -0500

    docs(accommodations): add documentation to GetRoomTypeByIdQuery

--- BackendAwSmartstay.API/Accommodations/Domain/Model/Queries/GetRoomTypeByIdQuery.cs
+++ BackendAwSmartstay.API/Accommodations/Domain/Model/Queries/GetRoomTypeByIdQuery.cs
@@ -1,4 +1,8 @@
 namespace BackendAwSmartstay.API.Accommodations.Domain.Model.Queries;
 
+/// <summary>
+/// Query to retrieve all rooms for a specific hotel.
+/// </summary>
+/// <param name="RoomTypeId"></param>
 public record GetRoomTypeByIdQuery(int RoomTypeId);
 
