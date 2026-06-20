4b1e0d598f15f2171a874a739d82fb355ac69fbb
u20211d760 <u20211d760@upc.edu.pe>
2026-06-20 11:10:23 -0500

    docs(accommodations): add documentation to create room type command.

--- BackendAwSmartstay.API/Accommodations/Domain/Model/Commands/CreateRoomTypeCommand.cs
+++ BackendAwSmartstay.API/Accommodations/Domain/Model/Commands/CreateRoomTypeCommand.cs
@@ -1,5 +1,8 @@
 namespace BackendAwSmartstay.API.Accommodations.Domain.Model.Commands;
 
-
+/// <summary>
+/// Command to create a new room type.
+/// </summary>
+/// <param name="Name"></param>
+/// <param name="Description"></param>
 public record CreateRoomTypeCommand(string Name, string Description);
-
